821a0659a7543e6fb0a85223a98ac2d8fcb69a8f
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-20 18:09:38 +0300

    Create a func to count all words

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-let words = { 
-	...verbs,
-	...nouns,
-	...adjectives,
-	...adverb,
-	...prepositions,
-	...pronouns };
+// let words = { 
+// 	...verbs,
+// 	...nouns,
+// 	...adjectives,
+// 	...adverb,
+// 	...prepositions,
+// 	...pronouns };
+
+
+const words = {
+	verb: verbs,
+	noun: nouns,
+	adjective: adjectives,
+	adverb: adverb,
+	preposition: prepositions,
+	pronoun: pronouns
+};
+
 
 let counter = document.querySelector('.counter');
-counter.innerHTML = `words amount: ${Object.keys(words).length}`;
+counter.innerHTML = `words amount: ${getWordCount(words)}`;
 
 search.oninput = function (e) {
-	let input = e.target.value;
-	if (input in words) {
-		data.innerHTML = `${input}: ${words[input]}`;
-	} else {
-		data.innerHTML = '';
+	// let input = e.target.value;
+	// if (input in words) {
+	// 	data.innerHTML = `${input}: ${words[input]}`;
+	// } else {
+	// 	data.innerHTML = '';
+	// }
+}
+
+
+function getWordCount(words) {
+	let result = 0;
+	for (let part in words) {
+		result += Object.keys(words[part]).length;
 	}
+	return result;
 }
