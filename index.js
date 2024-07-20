c237cdf59e11da70b38cf72f44ea2fe4255dc8f2
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-20 18:30:09 +0300

    Create search word func

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// let words = { 
-// 	...verbs,
-// 	...nouns,
-// 	...adjectives,
-// 	...adverb,
-// 	...prepositions,
-// 	...pronouns };
 
 
 const words = {
@@ -20,20 +13,35 @@ const words = {
 let counter = document.querySelector('.counter');
 counter.innerHTML = `words amount: ${getWordCount(words)}`;
 
-search.oninput = function (e) {
-	// let input = e.target.value;
-	// if (input in words) {
-	// 	data.innerHTML = `${input}: ${words[input]}`;
-	// } else {
-	// 	data.innerHTML = '';
-	// }
+document.getElementById('search').oninput = function (e) {
+	const input = e.target.value.trim();
+	const match = search(input, words);
+	console.log(match);
 }
+
+
 
 
 function getWordCount(words) {
 	let result = 0;
 	for (let part in words) {
 		result += Object.keys(words[part]).length;
+	}
+	return result;
+}
+
+
+function search(word, words) {
+	const result = {};
+	for (let part in words) {
+		const match = []
+		const data = words[part];
+		for (let w in data) {
+			if (w === word) {
+				match.push([word, data[w]])
+			}
+		}
+		if (match.length) result[part] = match;
 	}
 	return result;
 }
