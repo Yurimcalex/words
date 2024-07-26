7023c6694e7390070974f50b1e6db24286cfb1ea
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-26 20:19:31 +0300

    update:NewWords - add get new words func

--- src/NewWords.jsx
+++ src/NewWords.jsx
@@ -1,16 +1,56 @@
+import { useState } from 'react';
+import { db } from './db/db.js';
+
+
 export default function NewWords() {
+	const [text, setText] = useState('');
+	const [newWords, setNewWords] = useState([]);
+
+	const handleClick = () => {
+		const parts = divideIntoParts(text);
+		const words = getWords(parts);
+		const newWords = eliminateRepeat(db.getNewWords(words.map(toLowerCase)));
+	
+		console.log(newWords);
+	};
+
 	return (
 		<div className="new-words">
 			<div>
-				<textarea></textarea>
-				<button>Get words</button>
+				<textarea
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				></textarea>
+				
+				<button onClick={handleClick}>Get words</button>
 			</div>
 			
-			<div className="new-words_result"></div>
+			<div className="new-words_result">{text}</div>
 			
 			<div className="new-words_counter">
-				<div>New words count: 0</div>
+				<div>New words count: {newWords.length}</div>
 			</div>
 		</div>
 	);
+}
+
+
+function divideIntoParts(textInput) {
+	return textInput
+		.split('.').join(' . ')
+		.split(',').join(' , ')
+		.split('\n').join(' ')
+		.split(' ').filter(word => !!word);
+}
+
+function getWords(parts) {
+	return parts.filter(part => part.length > 1);
+}
+
+function eliminateRepeat(words) {
+	return Array.from(new Set(words));
+}
+
+function toLowerCase(word) {
+	return word[0].toLowerCase() + word.slice(1);
 }
