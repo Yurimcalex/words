2ff70a59147dddda7253c27157e75e270268a559
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-27 23:27:28 +0300

    refact:rename words-... class to page1-...

--- src/page1/Page1.jsx
+++ src/page1/Page1.jsx
@@ -9,9 +9,9 @@ export default function Page1() {
 	const [searchText, setSearchText] = useState('fundamental');
 
 	return (
-		<div className="words">
-			<div className="words_wrapper">
-				<div className="words_search">
+		<div className="page1">
+			<div className="page1_wrapper">
+				<div className="page1_search">
 					<Search 
 						text={searchText}
 						onChangeText={(e) => setSearchText(e.target.value)}
