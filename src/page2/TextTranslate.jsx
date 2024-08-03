64029fb0f6d82c7f30e9ece076988a7c37e434e0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-08-03 20:08:50 +0300

    style:page 2 when getting translation

--- src/page2/TextTranslate.jsx
+++ src/page2/TextTranslate.jsx
@@ -1,7 +1,10 @@
 export default function TextTranslate({ theme, text, onBtnClick }) {
 	return (
 		<div className="text-translate">
-			<div className={`roboto-lt fz-bgr lh-bgr`}>
+			<div 
+				className={`${theme}-2 roboto-lt fz-bgr lh-bgr`}
+				style={{ background: 'transparent' }}
+			>
 				{text}
 			</div>
 
