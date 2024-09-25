c511a795b33cf6055177b833cab8c8653949677e
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-09-25 20:58:47 +0300

    Update UserTextInput - add loading state

--- src/page2/UserTextInput.jsx
+++ src/page2/UserTextInput.jsx
@@ -1,10 +1,10 @@
-export default function UserTextInput({ text, onTextChange, onSubmit, theme }) {
+export default function UserTextInput({ text, isLoading, onTextChange, onSubmit, theme }) {
 	return (
 		<div className="user-text">
 			<textarea
 				placeholder="Put text here and get words you don't have in database."
 				className={`${theme}-2 fz-smr`}
-				value={text}
+				value={isLoading ? '...wait for translation' : text}
 				onChange={onTextChange}
 			>
 			</textarea>
@@ -12,6 +12,7 @@ export default function UserTextInput({ text, onTextChange, onSubmit, theme }) {
 			<div className={`${theme}-btn`}>
 				<button 
 					className={`${theme}-btn`}
+					disabled={isLoading}
 					onClick={onSubmit}
 				>
 					Get words
