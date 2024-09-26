4331e85a4f15bb415cdfd93d7cbeb3ffcd74c434
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-09-26 16:46:47 +0300

    Update HighlughtUserText component - use get parts feat to highligh words

--- src/page2/HighlightedUserText.jsx
+++ src/page2/HighlightedUserText.jsx
@@ -1,41 +1,24 @@
 import { Fragment } from 'react';
+import { getParts } from './useTranslation.js';
 
 export default function HighlightedUserText({ text, words, word, theme }) {
-	return (
-		<div className={`words-result ${theme}-dim lh-bgr`}>
-			{words.length > 0 && text
-				.split(' ')
-				.map((part, ind) => {
-					const match = matchWords(part, words);
-					if (match.length) {
-						return (
-							<Fragment key={part + ind}>
-								<span
-									className={match[0].toLowerCase() === word 
-										? `${theme}-highlight roboto-med-i` 
-										: `${theme}-select`}
-								>
-									{match[0]}
-								</span>
-								{match[1]} {' '}
-							</Fragment>
-						);
-					} else {
-						return part + ' ';
-					}
-				})}
-		</div>
-	);
-}
+ 	return (
+ 		<div className={`words-result ${theme}-dim lh-bgr`}>
+ 			{words.length > 0 && getParts(text)
+ 				.map((part, ind) => {
+ 					const lcPart = part.toLowerCase();
+ 					
+ 					if (lcPart === word) {
+ 						return <span key={part + ind} className={`${theme}-highlight roboto-med-i`}>{word}</span>
+ 					
+ 					} else if (words.includes(lcPart)) {
+ 						return <span key={part + ind} className={`${theme}-select`}>{part}</span>
+ 					
+ 					} else {
+ 						return <Fragment key={part + ind}>{part}</Fragment>;
+ 					}
 
-
-function matchWords(part, words) {
-	for (let i = 0; i < words.length; i += 1) {
-		const word = words[i];
-		const len = word.length;
-		if (part.toLowerCase().includes(word)) {
-			return [part.slice(0, len), part.slice(len)];
-		}
-	}
-	return [];
+ 				})}
+ 		</div>
+ 	);
 }
